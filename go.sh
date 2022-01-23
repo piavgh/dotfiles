b78a64d251ea0d40427d5463944954b6ca356a94
Hoang Trinh <hoang.trinhj@gmail.com>
2022-01-23 18:40:26 +0700

    feat: Add cgapp

--- go.sh
+++ go.sh
@@ -1,3 +1,5 @@
 go install github.com/securego/gosec/v2/cmd/gosec@latest
 
 go install github.com/go-critic/go-critic/cmd/gocritic@latest
+
+go install github.com/create-go-app/cli/v3/cmd/cgapp@latest
