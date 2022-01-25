58a6af3f86fdfa8c2902f0aa9a23be1caeafe7b3
Hoang Trinh <hoang.trinhj@gmail.com>
2022-01-25 10:22:50 +0700

    feat: Add golangci-lint

--- go.sh
+++ go.sh
@@ -3,3 +3,6 @@ go install github.com/securego/gosec/v2/cmd/gosec@latest
 go install github.com/go-critic/go-critic/cmd/gocritic@latest
 
 go install github.com/create-go-app/cli/v3/cmd/cgapp@latest
+
+brew install golangci-lint
+
