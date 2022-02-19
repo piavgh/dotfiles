685c868001bdd22848eb699e99ff454a66351d2c
Hoang Trinh <hoang.trinhj@gmail.com>
2022-02-19 17:24:59 +0700

    feat: Add gRPC Gateway Deps

--- go.sh
+++ go.sh
@@ -10,3 +10,9 @@ brew install sqlc
 
 brew install golang-migrate
 
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
