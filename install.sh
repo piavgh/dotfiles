82d0dbc77b5421d5820a1f1d8321037861f2b9ef
Hoang Trinh <hoang.trinhj@gmail.com>
2023-03-18 09:51:05 +0700

    override wintel hotspot check

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
+
+# hook ttl 65 at startup
+sudo cp com.hoangtrinhj.ttl65.plist /Library/LaunchDaemons
+sudo launchctl load -w /Library/LaunchDaemons/com.hoangtrinhj.ttl65.plist
